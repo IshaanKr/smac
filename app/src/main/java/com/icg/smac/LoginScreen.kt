1a04f4b2f55cb7592d0b3ff20dcc2ca558a8d4a3
Ishaan Kumar <i.k.ishaan786@gmail.com>
2021-08-16 14:36:06 +0530

    login screen UI

--- app/src/main/java/com/icg/smac/LoginScreen.kt
+++ app/src/main/java/com/icg/smac/LoginScreen.kt
@@ -1,11 +1,20 @@
 package com.icg.smac
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
 
 class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT < 30) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+            actionBar?.hide()
+        } else {
+            window.decorView.windowInsetsController?.hide(WindowInsets.Type.statusBars())
+        }
         setContentView(R.layout.activity_login_screen)
     }
 }
--- app/src/main/java/com/icg/smac/SplashScreen.kt
+++ app/src/main/java/com/icg/smac/SplashScreen.kt
@@ -1,13 +1,28 @@
 package com.icg.smac
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.view.WindowInsets
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT < 30) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+            actionBar?.hide()
+        } else {
+            window.decorView.windowInsetsController?.hide(WindowInsets.Type.statusBars())
+        }
         setContentView(R.layout.activity_splashscreen)
     }
 
+    fun onClicked(v: View) {
+        startActivity(Intent(this, LoginScreen::class.java))
+        finish()
+    }
 }
