ac9ce06346716b68b6d3869c91e34cdc0bc2501a
Ishaan Kumar <i.k.ishaan786@gmail.com>
2021-08-24 15:53:18 +0530

    changed dashboard fragment

--- app/src/main/java/com/icg/smac/MainScreen.kt
+++ app/src/main/java/com/icg/smac/MainScreen.kt
@@ -2,16 +2,29 @@ package com.icg.smac
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.icg.smac.databinding.ActivityMainscreenBinding
 
 class MainScreen : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainscreenBinding
+    private lateinit var binding: ActivityMainscreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val drawerLayout = binding.root
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        binding.toolbarLayout.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        binding.navView.setupWithNavController(navController)
 
     }
 }
